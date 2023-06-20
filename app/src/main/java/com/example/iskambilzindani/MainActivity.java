5cfca81c9fbc15756c94877c6ba6d66928889627
Theieyrre <crizftfm@gmail.com>
2023-06-20 19:27:44 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/iskambilzindani/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.iskambilzindani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    /* TODO Spinnerları tanımla */
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
